87481db7f20efcaa6f3051bb286dc47e031e896c
bururun <bururun@users.noreply.github.com>
2026-01-13 12:14:31 +0200

    improve: add unit tests

--- lib/helpers.py
+++ lib/helpers.py
@@ -7,3 +7,14 @@ def helper_function_1(x):
 def format_output(data):
     """Format output data."""
     return str(data).upper()
+
+
+# Helper functions
+
+def helper_function_14(x):
+    """Helper function for iteration 14."""
+    return x * 14
+
+def format_output(data):
+    """Format output data."""
+    return str(data).upper()
