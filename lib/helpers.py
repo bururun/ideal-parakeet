0465e30803b84a67eeb75330c7233fe57b7c8d00
bururun <bururun@users.noreply.github.com>
2026-01-26 11:06:33 +0200

    test: security enhancement

---
+++ lib/helpers.py
@@ -0,0 +1,9 @@
+# Helper functions
+
+def helper_function_1(x):
+    """Helper function for iteration 1."""
+    return x * 1
+
+def format_output(data):
+    """Format output data."""
+    return str(data).upper()
